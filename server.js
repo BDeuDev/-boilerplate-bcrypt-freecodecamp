09c4f9f816202979697b2b22bb9384715b67ed32
BDeuDev <bdeusich.dev@gmail.com>
2025-08-15 18:48:54 -0300

    test

--- server.js
+++ server.js
@@ -9,11 +9,25 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
-app.get('/', (req, res) => {
-  res.send("Hello world!");
-});
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err) return console.error(err);
+
+  console.log(hash); // acá verás el hash en consola
+
+  // comparar contra el mismo texto (debería dar true)
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    if (err) return console.error(err);
+    console.log(res); // true
+
+    // Si querés probar el false, descomenta esto:
+    // bcrypt.compare(someOtherPlaintextPassword, hash, (err, res2) => {
+    //   if (err) return console.error(err);
+    //   console.log(res2); // false
+    // });
+  });
+});
 
 
 //END_ASYNC
